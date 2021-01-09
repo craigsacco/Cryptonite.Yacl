82ca00bb275c845f2971e55ed5f2274ec2051823
Craig Sacco <craig.sacco@gmail.com>
2021-01-09 13:21:27 +1100

    fixed compilation failure, disabling C++/CLI features on non-Windows platforms

--- cmake.sh
+++ cmake.sh
@@ -1,8 +1,13 @@
-#!env bash
+#!/bin/bash
 
 set -e
 
 mkdir -p build
 pushd build
-cmake ../src
+if [[ "$OS" == "Windows_NT" ]]; then
+    cmake ../src
+else
+    # C++/CLI not supported on non-Windows platforms - disable BZip2 and XZ
+    cmake -DYACL_BZIP2=OFF -DYACL_XZ=OFF ../src
+fi
 popd
